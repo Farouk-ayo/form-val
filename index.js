1ee74f69b7b801f27ff3a3d250ee3720f26f20a3
Farouk-ayo <mustaphafarouk41@gmail.com>
2023-11-12 12:05:58 +0100

    multiselect

--- index.js
+++ index.js
@@ -402,9 +402,9 @@ function setupInput(inputElement, suggestionsElement, arrowElement) {
     suggestions.forEach((li) => {
       const suggestionText = li.textContent.toLowerCase();
       if (suggestionText.includes(filterText)) {
-        li.style.display = "block"; // Show the suggestion
+        li.style.display = "block";
       } else {
-        li.style.display = "none"; // Hide the suggestion
+        li.style.display = "none";
       }
     });
   }
@@ -541,72 +541,59 @@ const apiKey =
 const baseId = "app7VXdu27jaUQjIV";
 const tableId = "tblWxjOgN18FFMQ6k";
 const airtableApiUrl = `https://api.airtable.com/v0/${baseId}/${tableId}`;
-const populateDropdown = async (elementId, tableName) => {
-  try {
-    const response = await fetch(`${airtableApiUrl}`, {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      method: "GET",
-    });
-    const data = await response.json();
-    console.log(data);
+// const populateDropdown = async () => {
+//   try {
+//     const response = await fetch(`${airtableApiUrl}`, {
+//       headers: {
+//         accept: "application/json",
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//       method: "GET",
+//     });
+//     const data = await response.json();
 
-    // Now you can populate the dropdown with the retrieved data
-    const selectElement = document.getElementById(elementId);
-    data.records.forEach((record) => {
-      let option = document.createElement("option");
-      option.value = record.id;
-      option.textContent = record.fields["Name"];
-      selectElement.appendChild(option);
-    });
-    console.log(data.records);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-};
+//     console.log(data.records);
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// };
+// populateDropdown();
 
-function addNewEntry(tableName, entryName, callback) {
-  fetch(`${airtableApiUrl}/${tableName}`, {
+function addNewEntry(formData) {
+  const role = formData.get("role");
+  const sourceLink = formData.get("sourceLink");
+  const promptCatg = formData.get("promptCatg");
+  const promptTitle = formData.get("promptTitle");
+  const promptText = formData.get("promptText");
+  const arrayCatg = promptCatg.split(", ");
+
+  fetch(`${airtableApiUrl}`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${apiKey}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ fields: { Name: entryName } }),
+    body: JSON.stringify({
+      fields: {
+        "Prompt Category": arrayCatg,
+        "Prompt Text": promptText,
+        "Prompt Title": promptTitle,
+        "Prompt Link": sourceLink,
+
+        Role: [role],
+      },
+    }),
   })
     .then((response) => response.json())
     .then((data) => {
-      callback(data);
+      console.log("Success:", data);
     })
     .catch((error) => console.error("Error:", error));
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Populate dropdowns when the document is loaded
-  populateDropdown("promptCategory", "Categories");
-  populateDropdown("promptType", "Prompt Types");
-  populateDropdown("industry", "Industry");
-  populateDropdown("department", "Departments");
-  // ... add more dropdown populations as needed
-  // Event listeners for adding new entries
-  document.getElementById("addCategory").addEventListener("click", function () {
-    const newCategory = prompt("Enter new category name:");
-    if (newCategory) {
-      addNewEntry("Categories", newCategory, function (data) {
-        // Update the 'promptCategory' dropdown
-        populateDropdown("promptCategory", "Categories");
-      });
-    }
-  });
-  // ... similar event listeners for other add buttons
-  // Handle form submission
-  document
-    .getElementById("promptForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      // Handle form data submission to Airtable
-      // ... this would be similar to the createRecord function in the previous example
-    });
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  addNewEntry(formData);
 });
